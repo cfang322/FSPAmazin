9f9a6a9f567c086509e81dd7f9c338030a36a0e6
Yaqi Fang <cfang322@gmail.com>
2024-01-12 17:47:23 -0500

    Finish frontend auth setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 import configureStore from "./store/store";
+import csrfFetch, { restoreCSRF } from './store/csrf';
 
 const store = configureStore();
 
 if (import.meta.env.MODE !== "production") {
+  restoreCSRF();
   window.store = store;
-}
+  window.csrfFetch = csrfFetch;
+};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -18,3 +21,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </Provider>
   </React.StrictMode>
 );
+
