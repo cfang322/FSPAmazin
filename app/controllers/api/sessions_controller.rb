7b1bf2cd5d360ec7a193dc47de950392301f9c07
Yaqi Fang <cfang322@gmail.com>
2024-01-12 15:25:47 -0500

    Finish user auth, backend

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,8 +1,9 @@
 class Api::SessionsController < ApplicationController
   def show
-    @user = current_user
-    if @user
-      render json: @user
+    # banana
+    if current_user
+      @user = current_user
+      render 'api/users/show'
     else
       render json: { user: nil }
     end 
@@ -15,7 +16,7 @@ class Api::SessionsController < ApplicationController
 
     if @user
       login!(@user)
-      render json: @user
+      render 'api/users/show'
     else
       render json: { errors: ['credentials invalid!'] }, status: :unauthorized 
     end 
