f84f517ad1c843644f4ff4fee711739677e95244
Yaqi Fang <cfang322@gmail.com>
2024-01-17 14:54:04 -0500

    Finish NavBar, finish styling and put working links and icons

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,11 +1,11 @@
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
-// import cart from '../../images/cart.png';
-// import arrow from '../../images/arrow.png';
-// import git from '../../images/github.png';
-// import linkedin from '../../images/linkedin.png';
 import './Navigation.css';
+import github from '../../images/github.png';
+import linkedin from '../../images/linkedin.png';
+import logo from '../../images/logo.png';
+import cart from '../../images/cart.png';
 
 function Navigation() {
     const sessionUser = useSelector((state) => state.session.user);
@@ -25,7 +25,7 @@ function Navigation() {
         </li>
     </>
     ) : (
-    <>
+    <> 
         <li className='dropdwnSign'>
             <NavLink id='dropdwnLoginBtn' to='/login'>Log in</NavLink>
         </li>
@@ -37,19 +37,43 @@ function Navigation() {
     
     return (
     <>
-        <ul className='navBar'>
-            <div className='home'>
+        <div className='navBar'>
+            <div className='git'>
+                <a href="https://github.com/cfang322">
+                    <img src={github} alt="github-link" />
+                </a>
             </div>
-            <li className="dropdown">
-            <button className="dropbtn">{sessionUser ? `Hello, ${sessionUser.username}` : "Hello, sign in"}</button>
-            <div className="dropdown-content">
-                {sessionLinks}
+            <div className='linkedin'>
+                <a href="https://www.linkedin.com/in/yaqi-fang-125807250/">
+                    <img src={linkedin} alt="linkedin-link" />
+                </a>
             </div>
-            </li>
-        </ul>
-        <ul className='lowerNavBar'>
             
-        </ul>
+            <div className='home'>
+                <NavLink to="/">
+                    <img src={logo} alt="amazon-logo"/>
+                </NavLink>
+            </div>
+            
+            <li className="dropdown">
+                <button className="dropbtn">{sessionUser ? `Hello, ${sessionUser.username}` : "Hello, sign in"}</button>
+                <div className="dropdown-content">
+                    {sessionLinks}
+                </div>
+            </li>
+            
+            <div className='cart'>
+                <NavLink to="" id="cartLink">
+                    <p className='cartNumber'>0</p>
+                    <img src={cart} alt="cart-img"/>
+                    <p className="cartWord">Cart</p>
+                </NavLink>
+            </div>
+        </div>
+        
+        <div className='lowerNavBar'>
+        
+        </div>
     </>
     );
 }
