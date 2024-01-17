400c8699bc60eead8d2b91a49279c680bb7749b1
Yaqi Fang <cfang322@gmail.com>
2024-01-17 15:29:05 -0500

    Complete products routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
-
-  # post 'api/test', to: 'application#test'
+  
+  post 'api/test', to: 'application#test'
   
   namespace :api, defaults: { format: :json } do
     resources :users, only: :create
+    resources :products, only: [:index, :show]
     resource :session, only: [:show, :create, :destroy]
   end
 end
