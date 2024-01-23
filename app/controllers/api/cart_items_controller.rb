7fd9fd36f73710e35fd4a9478a7275eb15e6348c
Yaqi Fang <cfang322@gmail.com>
2024-01-23 16:17:51 -0500

    Finish view/index show page for cart_items

--- app/controllers/api/cart_items_controller.rb
+++ app/controllers/api/cart_items_controller.rb
@@ -42,4 +42,4 @@ class API::CartItemsController < ApplicationController
     def cart_item_params 
         params.require(:cart_item).permit(:id, :quantity, :user_id, :product_id)
     end
-end
+end  
