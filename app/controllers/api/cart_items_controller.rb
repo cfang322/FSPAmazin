e4a52cbae853f4d29721935efe1d1e799bd0e2ed
Yaqi Fang <cfang322@gmail.com>
2024-01-23 16:03:45 -0500

    fixed cart_items controller, added wrap parameters and edited update method

---
+++ app/controllers/api/cart_items_controller.rb
@@ -0,0 +1,41 @@
+class API::CartItemsController < ApplicationController
+    wrap_parameters include: CartItem.attribute_names + ['quantity', 'userId', 'productId']
+
+    def index 
+        @cart_items = current_user.cart_items 
+        render :index
+    end 
+
+
+    def create 
+        @cart_item = CartItem.new(cart_item_params)
+        if @cart_item.save 
+            render :show
+        else 
+            render json: @cart_item.errors.full_messages, status: :unprocessable_entity
+        end
+    end 
+    
+    def destroy 
+        @cart_item = CartItem.find(params[:id])
+        if @cart_item 
+            @cart_item.delete 
+            head :no_content 
+        end
+    end 
+
+    def update 
+        @cart_item = CartItem.find(params[:id])
+        if @cart_item 
+            if @cart_item.update(cart_item_params)
+                render :show
+            else 
+                render json: @cart_item.errors.full_messages, status: unprocessable_entity
+            end 
+        end
+    end
+
+    def cart_item_params 
+        params.require(:cart_item).permit(:id, :quantity, :user_id, :product_id)
+    end
+end
