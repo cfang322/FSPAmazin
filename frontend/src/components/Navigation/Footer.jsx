88ca8988541a96a2786e6a5060e75c2562d9e2d2
Yaqi Fang <cfang322@gmail.com>
2024-02-22 16:34:42 -0500

    Finish footer

--- frontend/src/components/Navigation/Footer.jsx
+++ frontend/src/components/Navigation/Footer.jsx
@@ -9,30 +9,28 @@ function Footer() {
             behavior: 'smooth',
         });
     };
-
+    
     return (
-        <>
-            <div className='footerDiv'>
-                <ul className='upperFooter' onClick={scrollToTop}>
-                    <p className='backToTopP'>Back to Top</p>
-                </ul>
-                <ul className='bottomFooter'>
-                    <div className='loginLinks'>
-                        <span className='loginGit'>
-                            <a href="https://github.com/cfang322">
-                                <img src={git} alt="github-link" />
-                            </a>
-                        </span>
-                        <span className='loginLinkedin'>
-                            <a href="https://www.linkedin.com/in/yaqi-fang-125807250/">
-                                <img src={linkedin} alt="linkedin-link" />
-                            </a>
-                        </span>
-                        <p className='loginLinkP'>Amazin 2024</p>
-                    </div>
-                </ul>
+        <div className='footerDiv'>
+            <div className='upperFooter' onClick={scrollToTop}>
+                <p className='backToTopP'>Back to Top</p>
             </div>
-        </>
+            <div className='bottomFooter'>
+                <div className='linksDiv'>
+                    <div className='git'>
+                        <a href="https://github.com/cfang322">
+                            <img src={git} alt="github-link" />
+                        </a>
+                    </div>
+                    <div className='linkedin'>
+                        <a href="https://www.linkedin.com/in/yaqi-fang-125807250/">
+                            <img src={linkedin} alt="linkedin-link" />
+                        </a>
+                    </div>
+                    <p className='loginLinkP'>Amazin 2024</p>
+                </div>
+            </div>
+        </div>
     );
 }
 
